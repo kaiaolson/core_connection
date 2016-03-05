49da9c1a7dbd6cf4712298ee3c08a168c216e072
kaiaolson <kaiaolson@gmail.com>
2016-03-04 22:22:58 -0800

    slim down CRUD structure for controllers that will only need create, update, destroy

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,29 +1,10 @@
 class EducationsController < ApplicationController
-  def index
-
-  end
-
-  def new
-
-  end
-
   def create
-
-  end
-
-  def show
-
-  end
-
-  def edit
-
   end
 
   def update
-
   end
 
   def destroy
-
   end
 end
