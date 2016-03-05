818bc0cb7fe9084ca2faa405248cdd2815e1f5a6
Stephen Charaton <charaton@users.noreply.github.com>
2016-03-05 13:24:22 -0800

    add educations controller, remove #new from links controller

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,10 +1,39 @@
 class EducationsController < ApplicationController
   def create
+    @profile = current_user_profile
+
+    if @education = Education.create education_params
+      redirect_to profile_path(@profile), notice: "Education added!"
+    else
+      flash[:alert] = "Error adding education!"
+      render new_profile_link(@profile)
+    end
+
   end
 
   def update
+    @profile = current_user_profile
+    @education = Education.find params[:id]
+
+    if @education.update education_params
+      redirect_to profile_path(@profile), notice: "Education updated!"
+    else
+      flash[:alert] = "Error updating education!"
+      render new_profile_link(@profile)
+    end
+
   end
 
   def destroy
+    @profile = current_user_profile
+    @education = params[:id]
+
+    @education.destroy
+    redirect_to profile_path(@profile)
   end
+
+  private
+
+  def education_params
+    params.require(:education).permit(:school, :image, :description, :school_url, :degree, :profile_id)
 end
