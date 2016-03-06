0822f3ce6c74f3ab6497cb05bfd981269a9eb25c
Stephen Charaton <charaton@users.noreply.github.com>
2016-03-05 16:28:32 -0800

    Delete Link model and add link columns to Profile

--- app/models/link.rb
+++
@@ -1,7 +0,0 @@
-class Link < ActiveRecord::Base
-  belongs_to :profile
-
-  validates :github_url, :url => {:allow_blank => true}
-  validates :twitter_url, :url => {:allow_blank => true}
-  validates :linkedin_url, :url => {:allow_blank => true}
-end
